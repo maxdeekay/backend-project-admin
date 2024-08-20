987ce034f7df1ee1d923cab266d6c503b7b8cea8
Max <max.assermark@gmail.com>
2024-08-20 05:52:28 +0200

    added update functionality

--- js/admin.js
+++ js/admin.js
@@ -7,19 +7,33 @@ window.onload = async () => {
     await checkAuth();
 
     document.getElementById("logout-button").onclick = () => {
-        // logout here
+        localStorage.removeItem("token");
+        window.location.href = "index.html";
     };
 
     document.getElementById("add-button").onclick = () => addConsumable();
     document.getElementById("cancel-button").onclick = () => resetCreate();
+
     // draws the menu items
     drawConsumables(await getMenu());
 }
 
-function addConsumable() {
-    // add consumable
+// function that adds a new consumable to the database
+async function addConsumable() {
+    resetCreate();
+
+    const container = document.getElementById("create-window");
+    container.style.display = "flex";
+
+    const form = document.getElementById("creation-form");
+    form.onsubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        handleConsumable(formData, "add", container);
+    }
 };
 
+// function that updates an existing consumable in the database
 function updateConsumable(consumable) {
     resetCreate();
 
@@ -27,43 +41,156 @@ function updateConsumable(consumable) {
     const title = document.getElementById("title");
     const ingredients = document.getElementById("ingredients");
     const price = document.getElementById("price");
+    const id = document.getElementById("id");
 
     title.value = consumable.title;
     ingredients.value = consumable.ingredients;
     price.value = consumable.price;
+    id.value = consumable._id;
 
     container.style.display = "flex";
 
-    // call function that does the API request with object and update/delete as parameters
+    const form = document.getElementById("creation-form");
+    form.onsubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        handleConsumable(formData, "update", container);
+    }
 }
 
-async function handleConsumables(consumable, action) {
-    // handle the API requests
+// used for adding and updating consumables. the function both validates inputs and sends API requests
+async function handleConsumable(form, action, container) {
+    const title = form.get("title");
+    const ingredients = form.get("ingredients").toLowerCase().split(",");
+    const price = form.get("price");
+    
+    // checking if ingredients is empty
+    const ingredientExist = !(ingredients.length === 1 && ingredients[0] === "");
+    
+    // validating input
+    if (!title || !ingredientExist || !price) {
+        const missingTitle = document.getElementById("title").classList.toggle("red-border", !title);
+        const MTT = document.getElementById("title-error-text");
+        MTT.innerHTML = missingTitle ? "Måste fyllas i" : "";
+        
+        const missingIngredients = document.getElementById("ingredients").classList.toggle("red-border", !ingredientExist);
+        const MIT = document.getElementById("ingredients-error-text");
+        MIT.innerHTML = missingIngredients ? "Måste fyllas i" : "";
 
-    /* const url = baseURL + "/admin/update/" + consumable._id;
-    const response = await fetch(url, {
+        const missingPrice = document.getElementById("price").classList.toggle("red-border", !price);
+        const MPT = document.getElementById("price-error-text");
+        MPT.innerHTML = missingPrice ? "Måste fyllas i" : "";
+        
+        return;
+    }
+    
+    // checks if price is a number
+    const isNumber = /^\d+$/.test(price);
+    if (!isNumber) {
+        document.getElementById("price").classList.toggle("red-border", !isNumber);
+        document.getElementById("price-error-text").innerHTML = "Får endast innehålla siffror";
+        return;
+    }
 
-    }); */
+    // add new consumable
+    if (action === "add") {
+        try {
+            const url = baseURL + "/admin/add";
+            const token = localStorage.getItem("token");
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer " + token
+                },
+                body: JSON.stringify({
+                    title: title,
+                    ingredients: ingredients,
+                    price: price
+                })
+            });
+
+            // if the consumable was added correctly
+            if (response.ok) {
+                drawConsumables(await getMenu());
+                container.style.display = "none";
+            } else {
+                console.log("Server error adding consumable");
+            }
+        } catch (error) {
+            console.log("Error adding consumable: " + error);
+        }
+    // update existing consumable
+    } else if (action == "update") {
+        try {
+            const id = form.get("id");
+            const url = baseURL + "/admin/update/" + id;
+            const token = localStorage.getItem("token");
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer " + token
+                },
+                body: JSON.stringify({
+                    title: title,
+                    ingredients: ingredients,
+                    price: price
+                })
+            })
+            
+            // if the consumable was updated correctly
+            if (response.ok) {
+                drawConsumables(await getMenu());
+                container.style.display = "none";
+            } else {
+                console.log("Server error updating consumable");
+            }
+        } catch (error) {
+            console.log("Error updating consumable: " + error);
+        }
+    }
 }
 
+// function to delete a consumable from the database
+async function deleteConsumable(id) {
+    try {
+        const url = baseURL + "/admin/delete/" + id;
+        const token = localStorage.getItem("token");
+        
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        });
+
+        // remove the deleted dish - finns det bättre sätt att göra utan att behöva ladda om?
+        if (response.ok) location.reload();
+    } catch (error) {
+        console.log("Error deleting consumable: " + error);
+    }
+}
+
+// function that draws all consumables, uses an object as argument
 function drawConsumables(consumables) {
     const container = document.getElementById("consumable-container");
+    container.innerHTML = "";
 
     consumables.consumables.forEach((consumable) => {
         const div = document.createElement("div");
         const title = document.createElement("h2");
         const ingr = document.createElement("p");
         const price = document.createElement("p");
+        const buttonDiv = document.createElement("div");
         const updateBtn = document.createElement("button");
         const deleteBtn = document.createElement("button");
 
-        updateBtn.onclick = () => {
-            updateConsumable(consumable);
-        }
+        // updates consumable in database, consumable object as parameter
+        updateBtn.onclick = () => updateConsumable(consumable);
 
-        deleteBtn.onclick = () => {
-            deleteConsumable(consumable._id);
-        }
+        // deletes consumable from database, objectID of consumable as parameter
+        deleteBtn.onclick = () => deleteConsumable(consumable._id);
 
         div.classList.add("consumable");
         title.classList.add("title");
@@ -87,12 +214,14 @@ function drawConsumables(consumables) {
         div.appendChild(title);
         div.appendChild(ingr);
         div.appendChild(price);
-        div.appendChild(updateBtn);
-        div.appendChild(deleteBtn);
+        buttonDiv.appendChild(updateBtn);
+        buttonDiv.appendChild(deleteBtn);
+        div.appendChild(buttonDiv);
         container.appendChild(div);
     });
 }
 
+// gets the current menu from the database
 async function getMenu() {
     const url = baseURL + "/consumables";
     const response = await fetch(url);
@@ -106,6 +235,7 @@ async function getMenu() {
     return response.ok ? await response.json() : console.log("Server error");
 } 
 
+// authorizes the user - redirects to index.html if not authorized
 async function checkAuth() {
     const url = baseURL + "/admin/view";
     const token = localStorage.getItem("token");
@@ -120,12 +250,20 @@ async function checkAuth() {
     if (!response.ok) window.location.href="index.html";
 }
 
+// resets the create-window used for creation of new consumables or updating of existing ones
 function resetCreate() {
     const create = document.getElementById("create-window");
-    const container = document.getElementById("create-window");
     const title = document.getElementById("title");
     const ingredients = document.getElementById("ingredients");
     const price = document.getElementById("price");
+
+    document.getElementById("title-error-text").innerHTML = "";
+    document.getElementById("ingredients-error-text").innerHTML = "";
+    document.getElementById("price-error-text").innerHTML = "";
+
+    title.classList.remove("red-border");
+    ingredients.classList.remove("red-border");
+    price.classList.remove("red-border");
 
     title.value = "";
     ingredients.value = "";
